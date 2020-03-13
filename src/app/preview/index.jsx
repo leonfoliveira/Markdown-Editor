07dcd00a6ce638a04f786c120517b839f8be70aa
Leonardo F Oliveira <leonfoliveira@gmail.com>
2020-03-13 20:33:36 -0300

    Preview Created

--- src/app/preview/index.jsx
+++ src/app/preview/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Converter } from 'react-showdown';
+
+import MarkdownArea from './markdownArea';
 
 export default () => {
+  const content = useSelector(state => state.doc.content);
+  const converter = new Converter({ tables: true, strikethrough: true });
+
   return (
     <div className="preview">
-      
+      <MarkdownArea
+        content={converter.convert(content)}
+      />
     </div>
   )
 }
---
+++ src/app/preview/markdownArea/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default props => (
+  <div className="preview-markdownarea">
+    {props.content}
+  </div>
+)
