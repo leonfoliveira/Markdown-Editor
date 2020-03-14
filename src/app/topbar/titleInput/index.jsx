274e14d95f0974086693211f11db67a5cdcd04e5
Leonardo F Oliveira <leonfoliveira@gmail.com>
2020-03-14 17:48:38 -0300

    Autosize title input

--- src/app/topbar/titleInput/index.jsx
+++ src/app/topbar/titleInput/index.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-export default props => (
-  <div className="title-input">
-    <input
-      type="text"
-      className="input"
-      value={props.title}
-      onChange={props.onChange}
-    />
-  </div>
-)
+export default props => {
+  const handleChange = e => {
+    props.onChange(e);
+
+    e.target.style.width = `${e.target.value.length + 1}ch`;
+  }
+
+  useEffect(() => {
+    let elem = document.getElementById('title-input');
+
+    elem.style.width = `${elem.value.length + 0.5}ch`;
+  });
+
+  return (
+    <div className="title-input">
+      <input
+        type="text"
+        className="input"
+        value={props.title}
+        onChange={handleChange}
+        id="title-input"
+      />.md
+    </div>
+  )
+}
